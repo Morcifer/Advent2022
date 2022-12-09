20d025c1fd0ec96e6ecc506e762c4d24d72dca13
morcifer <mor.verbin@gmail.com>
2022-12-09 18:51:08 +0100

    Day 9, Cleaner.

--- src/day9.py
+++ src/day9.py
@@ -9,13 +9,16 @@ def parser(s: List[str]) -> Tuple[str, int]:
     return s[0], int(s[1])
 
 
-def process_data(data: List[Tuple[str, int]]) -> List[Tuple[int, int]]:
-    head_x, head_y = 0, 0
-    tail_x, tail_y = 0, 0
-    visited_tail_spots = [(tail_x, tail_y)]
+def process_data(data: List[Tuple[str, int]], tails: int) -> List[Tuple[int, int]]:
+    knot_xs = [0 for _ in range(tails + 1)]
+    knot_ys = [0 for _ in range(tails + 1)]
+
+    visited_tail_spots = [(knot_xs[-1], knot_ys[-1])]
 
     for i, (direction, amount) in enumerate(data):
         for _ in range(amount):
+            head_x, head_y = knot_xs[0], knot_ys[0]
+
             if direction == "U":
                 head_y += 1
             elif direction == "D":
@@ -27,82 +30,42 @@ def process_data(data: List[Tuple[str, int]]) -> List[Tuple[int, int]]:
             else:
                 raise ValueError("?")
 
-            if abs(head_x - tail_x) <= 1 and abs(head_y - tail_y) <= 1:
-                continue
+            knot_xs[0], knot_ys[0] = head_x, head_y
 
-            if head_x > tail_x:
-                tail_x += 1
-            elif head_x < tail_x:
-                tail_x -= 1
+            for knot_index in range(1, tails + 1):
+                head_x, head_y = knot_xs[knot_index - 1], knot_ys[knot_index - 1]
+                tail_x, tail_y = knot_xs[knot_index],  knot_ys[knot_index]
 
-            if head_y > tail_y:
-                tail_y += 1
-            elif head_y < tail_y:
-                tail_y -= 1
-
-            visited_tail_spots.append((tail_x, tail_y))
-
-    return visited_tail_spots
-
-
-
-def process_data_2(data: List[Tuple[str, int]]) -> List[Tuple[int, int]]:
-    head_x, head_y = 0, 0
-    tail_xs = [0 for _ in range(9)]
-    tail_ys = [0 for _ in range(9)]
-
-    visited_tail_spots = [(tail_xs[-1], tail_ys[-1])]
-
-    for i, (direction, amount) in enumerate(data):
-        for _ in range(amount):
-            if direction == "U":
-                head_y += 1
-            elif direction == "D":
-                head_y -= 1
-            elif direction == "R":
-                head_x += 1
-            elif direction == "L":
-                head_x -= 1
-            else:
-                raise ValueError("?")
-
-            for tail_index in range(9):
-                relevant_head_x = head_x if tail_index == 0 else tail_xs[tail_index - 1]
-                relevant_head_y = head_y if tail_index == 0 else tail_ys[tail_index - 1]
-
-                tail_x = tail_xs[tail_index]
-                tail_y = tail_ys[tail_index]
-
-                if abs(relevant_head_x - tail_x) <= 1 and abs(relevant_head_y - tail_y) <= 1:
+                if abs(head_x - tail_x) <= 1 and abs(head_y - tail_y) <= 1:
                     continue
 
-                if relevant_head_x > tail_x:
+                if head_x > tail_x:
                     tail_x += 1
-                elif relevant_head_x < tail_x:
+                elif head_x < tail_x:
                     tail_x -= 1
 
-                if relevant_head_y > tail_y:
+                if head_y > tail_y:
                     tail_y += 1
-                elif relevant_head_y < tail_y:
+                elif head_y < tail_y:
                     tail_y -= 1
 
-                tail_xs[tail_index] = tail_x
-                tail_ys[tail_index] = tail_y
+                knot_xs[knot_index] = tail_x
+                knot_ys[knot_index] = tail_y
 
-            visited_tail_spots.append((tail_xs[-1], tail_ys[-1]))
+            visited_tail_spots.append((knot_xs[-1], knot_ys[-1]))
 
     return visited_tail_spots
 
 
 def part_1(is_test: bool) -> int:
     data = load_data(DAY, parser, "data", is_test=is_test)
-    result = process_data(data)
+    result = process_data(data, tails=1)
     return len(set(result))
 
 
 def part_2(is_test: bool) -> int:
     data = load_data(DAY, parser, "data", is_test=is_test)
-    result = process_data_2(data)
+    result = process_data(data, tails=9)
     return len(set(result))
 
 

272bed5e4507f2c7259cf04e28c4e9d022caea79
morcifer <mor.verbin@gmail.com>
2022-12-20 09:11:49 +0100

    Day 20: Why manipulate multiple lists if you can just use one? And also - modulu of one under, damnit!

--- src/day20.py
+++ src/day20.py
@@ -17,78 +17,51 @@ def parser(s: List[str]) -> int:
     return int(s[0])
 
 
-def process_data(data: List[int]) -> List[int]:
-    # We cannot assume the list is unique. We have to keep track of indices.
-    original_numbers = data[:]
-    original_indices = list(range(len(data)))
-    current_indices = list(range(len(data)))
+def process_data(data: List[int], key: int, times_to_mix: int) -> int:
+    data = [n * key for n in data]
 
-    for index in original_indices:
-        # print(data)
+    # We cannot assume the list is unique. We have to keep track of everything.
+    indexed_numbers = [(i, n) for i, n in enumerate(data)]
 
-        number = original_numbers[index]
-        old_index = current_indices[index]
+    for i in range(times_to_mix):
+        for index in range(len(data)):
+            current_index, original_index, number = next(
+                (current_index, original_index, number)
+                for current_index, (original_index, number) in enumerate(indexed_numbers)
+                if original_index == index
+            )
 
-        if data[old_index] != number:
-            print("There's an offset somewhere!")
+            new_index = (current_index + number) % (len(data) - 1)  # Because we remove _before_ shifting. :/
 
-        new_index = old_index + number
+            del indexed_numbers[current_index]
+            indexed_numbers.insert(new_index, (original_index, number))
 
-        if number < 0:
-            new_index = (new_index % len(data)) - 1
-
-        if new_index == -1:
-            # Coundary condition.
-            new_index = len(data)
-
-        if new_index > len(data):
-            new_index = (new_index % len(data)) + 1
-
-        print(f"Moving {number} (old index {index} out of {len(data)}) from {old_index} to {new_index}")
-
-        if new_index == old_index:
-            continue
-
-        for i in range(len(current_indices)):
-            if current_indices[i] > old_index:
-                current_indices[i] -= 1
-
-        for i in range(len(current_indices)):
-            if current_indices[i] > new_index - 1:
-                current_indices[i] += 1
-
-        current_indices[index] = new_index
-
-        del data[old_index]
-        data.insert(new_index, number)
-
-    return data
-
-
-def part_1(is_test: bool) -> int:
-    data = load_data(DAY, parser, "data", is_test=is_test)
-    result = process_data(data)
-
-    print(data)
+    numbers = [number for _, number in indexed_numbers]
 
     # Find the 0:
-    spot = next(i for i, n in enumerate(result) if n == 0)
+    spot = next(i for i, n in enumerate(numbers) if n == 0)
 
     # 1000th, 2000th, and 3000th
-    n1 = result[(spot + 1000) % len(result)]
-    n2 = result[(spot + 2000) % len(result)]
-    n3 = result[(spot + 3000) % len(result)]
+    n1 = numbers[(spot + 1000) % len(numbers)]
+    n2 = numbers[(spot + 2000) % len(numbers)]
+    n3 = numbers[(spot + 3000) % len(numbers)]
 
     return n1 + n2 + n3
 
 
+def part_1(is_test: bool) -> int:
+    data = load_data(DAY, parser, "data", is_test=is_test)
+    result = process_data(data, key=1, times_to_mix=1)
+    return result
+
+
 def part_2(is_test: bool) -> int:
     data = load_data(DAY, parser, "data", is_test=is_test)
-    result = process_data(data)
+    result = process_data(data, key=811589153, times_to_mix=10)
     return result
 
 
 if __name__ == '__main__':
-    is_test = True
+    is_test = False
     print(f"Day {DAY} result 1: {part_1(is_test)}")
-    # print(f"Day {DAY} result 2: {part_2(is_test)}")
+    print(f"Day {DAY} result 2: {part_2(is_test)}")
